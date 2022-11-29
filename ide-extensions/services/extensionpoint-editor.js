ee683e5b0f5886d3f683c7096845b5a5cdebcbed
StanZGenchev <stan.z.genchev@gmail.com>
2022-11-29 16:49:18 +0200

    Migrated editors to new system

--- ide-extensions/services/extensionpoint-editor.js
+++ ide-extensions/services/extensionpoint-editor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,16 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getEditor = function () {
-	return {
-		"id": "extensionpoint",
-		"name": "ExtensionPoint",
-		"factory": "frame",
-		"region": "center-top",
-		"label": "Extension Point",
-		"link": "../ide-extensions/editors/extensionpoint/editor.html",
-		"contentTypes": ["application/json+extension-point"]
-	};
+const editorData = {
+	id: "extensionpoint",
+	label: "Extension Point",
+	factory: "frame",
+	region: "center",
+	link: "../ide-extensions/editors/extensionpoint/editor.html",
+	contentTypes: ["application/json+extension-point"]
 };
+if (typeof exports !== 'undefined') {
+	exports.getEditor = function () {
+		return editorData;
+	}
+}
